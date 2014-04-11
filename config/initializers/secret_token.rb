407c2a3996392c237ed96b0d128bed1b679c3c75
Killavus <killavus@gmail.com>
2014-04-12 00:35:33 +0200

    Initial commit po raz n-ty

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+D3APIApp::Application.config.secret_key_base = 'ac93a24b6db9923b03204c086ea749bbde6af9cf94be8f3f07501a9ee242d43c3d0a6b8ba08c48cd74b5ab138004bb247160e5753e21f4d63136a0e2ee2d43d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+D3APIApp::Application.config.session_store :cookie_store, key: '_D3API-App_session'
