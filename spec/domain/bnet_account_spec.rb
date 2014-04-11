54af53bd7ff6ad287e88d133ddf0078926e88177
Killavus <killavus@gmail.com>
2014-04-12 01:26:29 +0200

    Account test

---
+++ spec/domain/bnet_account_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Bnet_Account do
+  
+  subject { described_class.new name: "Test", battle_tag: 1234 }
+  
+  describe '#name' do
+    it 'returns current account name' do
+      subject.name.should eq "Test"
+    end
+  end
+
+  describe "#battle_tag" do
+    it "returns current account BattleTag" do
+      subject.battle_tag.should eq 1234
+    end
+  end
+
+end
